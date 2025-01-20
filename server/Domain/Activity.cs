5245e42f4799b76f1932eeacd06773d5f7854acf
thinh tu <khangthinhproc@gmail.com>
2025-01-20 12:58:39 +0700

    first commit~

---
+++ server/Domain/Activity.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+   public class Activity
+   {
+      public Guid Id { get; set; }
+      public required string Title { get; set; }
+      public DateTime Date { get; set; }
+      public required string Description { get; set; }
+      public required string Category { get; set; }
+      public required string City { get; set; }
+      public required string Venue { get; set; }
+   }
+}
---
+++ server/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+   public class ApplicationDbContext : DbContext
+   {
+      public ApplicationDbContext(DbContextOptions options) : base(options)
+      {
+      }
+
+      public DbSet<Activity> Activities { get; set; }
+   }
+}
